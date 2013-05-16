428dcb3558a33c7ee7b6cf83c783b9c31b72178e
kazuto_yamasaki <kazutoyamasaki@Kazuto-no-MacBook-Pro.local>
2013-05-16 19:01:46 +0900

    ADD:OrderedDictionary Project.

---
+++ OrderedDictionary/OrderedDictionary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OrderedDictionary
+//
+//  Created by kazuto_yamasaki on 2013/05/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
