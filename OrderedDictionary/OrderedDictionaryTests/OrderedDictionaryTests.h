120b240d5e72533ccb64a545bc26f1c474e4290d
kazuto_yamasaki <kazutoyamasaki@Kazuto-no-MacBook-Pro.local>
2013-05-16 19:01:46 +0900

    ADD:OrderedDictionary Project.

---
+++ OrderedDictionary/OrderedDictionaryTests/OrderedDictionaryTests.h
@@ -0,0 +1,17 @@
+//
+//  OrderedDictionaryTests.h
+//  OrderedDictionaryTests
+//
+//  Created by kazuto_yamasaki on 2013/05/16.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@class OrderedDictionary;
+
+@interface OrderedDictionaryTests : SenTestCase
+
+@property (retain) OrderedDictionary *testDict;
+
+@end
